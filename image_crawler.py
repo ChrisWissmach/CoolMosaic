2f1f3aacab907b7f382e80023287df36cc1ec86e
Daniel Kou <danielkou131@gmail.com>
2018-07-07 23:41:00 -0700

    Crawl Google and Bing for images

---
+++ image_crawler.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup as bs
+from io import BytesIO
+from PIL import Image
+
+def crawlImages(keyword):
+    google_search = "https://www.google.com/search?q=" + keyword + "&source=lnms&tbm=isch&sa=X&ved=0ahUKEwilqcTb4Y7cAhUPHzQIHSTiCc8Q_AUICigB&biw=1440&bih=742"
+    bing_search = "https://www4.bing.com/images/search?q=" + keyword + "&FORM=HDRSC2"
+    images = []
+
+    response = requests.get(google_search, timeout=2)
+    soup = bs(response.content, "html.parser")
+    soup = soup.find("table", attrs={"class":"images_table"})
+
+    # google search
+    for row in soup.findAll("tr"):
+        for item in row.findAll("td"):
+            resp = requests.get(item.img.get("src"))
+            images.append(BytesIO(resp.content))
+
+
+    response = requests.get(bing_search, timeout=2)
+    soup = bs(response.content, "html.parser")
+
+    # bing search
+    for image in soup.findAll("div", attrs={"class":"cico"}):
+        resp = requests.get(image.img.get("src"))
+        images.append(BytesIO(resp.content))
+
+    return images
+
+# for testing purposes
+im = crawlImages("waterloo university")
+Image.open(im[0]).show()
