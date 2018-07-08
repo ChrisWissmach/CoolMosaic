7a0e18dd0b8d396c327a95dda15c06f785c6e995
Daniel Kou <danielkou131@gmail.com>
2018-07-08 01:17:52 -0700

    Add verbose mode

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ from utils.image_utils import getRGBArray, getAverageColour, getMostAppropriateI
 from utils.image_crawler import crawlImages
 
 
-def makeMosaic(im, keyword):
+def makeMosaic(im, keyword, verbose=False):
 	orig_width, orig_height = im.size
-	mosaic_images = crawlImages(keyword)
+	mosaic_images = crawlImages(keyword, verbose)
 
 	images_arr= []
+
+	if (verbose):
+		print "Creating image database"
 	for i in mosaic_images:
 		i2 = Image.open(i)
 		images_arr.append((getAverageColour(i2), i2))
@@ -26,10 +29,16 @@ def makeMosaic(im, keyword):
 
 	mosaic = Image.new('RGB', (new_width, new_height), 0)
 
+	processed = 1
+
 	for i in range(orig_height):
 		x1 = 0
 		x2 = tile_size
 		for j in range(orig_width):
+			if (verbose):
+				processed += 1
+				if (processed % 50 == 0):
+					print "Processing"
 			best_im = getMostAppropriateImage(im.getpixel((j,i)), images_arr)
 			mosaic.paste(best_im.resize((tile_size, tile_size)), (x1, y1))
 			x1 = x2 + 1
@@ -44,13 +53,14 @@ def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("file", help="image to create a mosaic for")
 	parser.add_argument("keyword", help="keyword of images that make up your mosaic")
+	parser.add_argument("-v", "--verbose", action="store_true", help="toggle log messages")
 	args = parser.parse_args()
 
 	# For testing :-)
 	# image_path = "hotdog.jpg"
 
 	im = Image.open(args.file)
-	makeMosaic(im, args.keyword)
+	makeMosaic(im, args.keyword, args.verbose)
 
 if __name__ == "__main__":
 	main()
--- utils/image_crawler.py
+++ utils/image_crawler.py
@@ -3,14 +3,18 @@ from bs4 import BeautifulSoup as bs
 from io import BytesIO
 from PIL import Image
 
-def crawlImages(keyword):
+def crawlImages(keyword, verbose=False):
     google_search = "https://www.google.com/search?q=" + keyword + "&source=lnms&tbm=isch&sa=X&ved=0ahUKEwilqcTb4Y7cAhUPHzQIHSTiCc8Q_AUICigB&biw=1440&bih=742"
     bing_search = "https://www4.bing.com/images/search?q=" + keyword + "&FORM=HDRSC2"
     images = []
 
+    if (verbose):
+        print "Crawling Google"
     response = requests.get(google_search, timeout=2)
     soup = bs(response.content, "html.parser")
     soup = soup.find("table", attrs={"class":"images_table"})
+
+    processed = 1
 
     # google search
     for row in soup.findAll("tr"):
@@ -18,7 +22,14 @@ def crawlImages(keyword):
             resp = requests.get(item.img.get("src"))
             images.append(BytesIO(resp.content))
 
+            if (verbose):
+                processed += 1
+                if (processed % 5 == 0):
+                    print "Downloading images"
 
+
+    if (verbose):
+        print "Crawling Bing"
     response = requests.get(bing_search, timeout=2)
     soup = bs(response.content, "html.parser")
 
@@ -26,6 +37,11 @@ def crawlImages(keyword):
     for image in soup.findAll("div", attrs={"class":"cico"}):
         resp = requests.get(image.img.get("src"))
         images.append(BytesIO(resp.content))
+
+        if (verbose):
+            processed += 1
+            if (processed % 5 == 0):
+                print "Downloading images"
 
     return images
 
