056679034d43dfb9d76678de6b43bc517afa31cd
Chris Wissmach <chriswissmach@gmail.com>
2018-07-07 22:58:22 -0700

    Create avg image colour function, move to utils file (#2)

---
+++ utils/image_utils.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+from PIL import Image
+
+
+def getRGBArray(im):
+	width, height = im.size
+	rgb_arr = [[(0,0,0) for i in range(width)] for i in range(height)]
+
+	for i in range(height):
+		for j in range(width):
+			# Reverse width/height for getpixel
+			rgb_arr[i][j] = im.getpixel((j,i))
+
+	return rgb_arr
+
+def getAverageColour(im):
+	rgb_array = getRGBArray(im)
+	width, height = im.size
+	avg_r = 0
+	avg_g = 0
+	avg_b = 0
+	for i in range(height):
+		for j in range(width):
+			r, g, b = rgb_array[i][j]
+			avg_r += r
+			avg_g += g
+			avg_b += b
+
+	num_pixels = width * height
+	return (avg_r / num_pixels, avg_g / num_pixels, avg_b / num_pixels)
